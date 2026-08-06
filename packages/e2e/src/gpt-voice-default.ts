5f07f2261a084b478033be281527fcc633d3acd0
Le Vivilet <levivilet@pm.me>
2026-08-06 15:10:43 +0200

    e2e

--- packages/e2e/src/gpt-voice-default.ts
+++ packages/e2e/src/gpt-voice-default.ts
@@ -2,7 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'gpt-voice.basic'
 
-export const test: Test = async ({ expect, Locator }) => {
+export const test: Test = async ({ expect, Locator, SideBar }) => {
+  await SideBar.open('gpt-voice.views.default')
   const main = Locator('.gpt-voice')
   await expect(main).toBeVisible()
 }
