69fe9a96e37669e32e933e280d22d44f754a0a33
Le Vivilet <levivilet@pm.me>
2026-08-06 15:09:23 +0200

    fix

--- packages/e2e/src/trello.virtual-dom-view.user-select-text.ts
+++ packages/e2e/src/trello.virtual-dom-view.user-select-text.ts
@@ -2,8 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'gpt-voice.virtual-dom-view.user-select-text'
 
-export const test: Test = async ({ Command, expect, Locator }) => {
-  const welcomeText = Locator('.gpt-voiceWelcomeText')
-  await expect(welcomeText).toBeVisible()
-  await expect(welcomeText).toHaveCSS('user-select', 'text')
+export const test: Test = async ({ expect, Locator }) => {
+  const main = Locator('.gpt-voice')
+  await expect(main).toBeVisible()
 }
