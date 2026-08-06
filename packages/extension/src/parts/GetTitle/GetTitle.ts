013f9090a0db69a7b7ccc4daaa530e22851d1435
Le Vivilet <levivilet@pm.me>
2026-08-06 15:00:22 +0200

    fix

--- packages/extension/src/parts/GetTitle/GetTitle.ts
+++ packages/extension/src/parts/GetTitle/GetTitle.ts
@@ -1,10 +1,3 @@
-import type { gpt-voiceViewState } from '../gpt-voiceViewState/gpt-voiceViewState.ts'
-import * as gpt-voiceStrings from '../gpt-voiceStrings/gpt-voiceStrings.ts'
-
-export const getTitle = (state: Readonly<gpt-voiceViewState>): string => {
-  const { boardDetail } = state
-  if (boardDetail) {
-    return gpt-voiceStrings.gpt-voiceBoard(boardDetail.board.name)
-  }
-  return gpt-voiceStrings.gpt-voice()
+export const getTitle = (state: any): string => {
+  return ''
 }
