665e72e153d879776e714a20da7e9ba3a6af1a58
Le Vivilet <levivilet@pm.me>
2026-08-06 18:00:56 +0200

    add bubble

--- packages/extension/src/parts/Render/Render.ts
+++ packages/extension/src/parts/Render/Render.ts
@@ -7,10 +7,37 @@ import {
 export const render = (): readonly VirtualDomNode[] => {
   return [
     {
-      childCount: 1,
-      className: 'gpt-voice',
+      childCount: 4,
+      className: 'GptVoice',
       type: VirtualDomElements.Div,
     },
-    text('hello world 123'),
+    {
+      type: VirtualDomElements.Div,
+      className: 'GptVoiceStage',
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'GptVoiceBubble',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'GptVoiceStatus',
+      childCount: 1,
+    },
+    text('idle'),
+    {
+      type: VirtualDomElements.Button,
+      className: 'GptVoiceButton',
+      id: 'toggle',
+      childCount: 1,
+    },
+    text('Start talking'),
+    {
+      type: VirtualDomElements.Div,
+      childCount: 0,
+      className: 'GptVoiceTranscript',
+    },
   ]
 }
