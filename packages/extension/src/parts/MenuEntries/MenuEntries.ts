4d0b327ef1a9fa6474da93b27afe5296fef9401b
Le Vivilet <levivilet@pm.me>
2026-08-06 15:11:53 +0200

    menu

--- packages/extension/src/parts/MenuEntries/MenuEntries.ts
+++ packages/extension/src/parts/MenuEntries/MenuEntries.ts
@@ -1,10 +1,7 @@
-import type { gpt-voiceViewState } from '../gpt-voiceViewState/gpt-voiceViewState.ts'
-import * as gpt-voiceStrings from '../gpt-voiceStrings/gpt-voiceStrings.ts'
-
-export const MenuIdBoard = 'gpt-voice.board'
-export const MenuIdCard = 'gpt-voice.card'
-export const MenuIdCardDetail = 'gpt-voice.cardDetail'
-export const MenuIdList = 'gpt-voice.list'
+export const MenuIdBoard = 'gptvoice.board'
+export const MenuIdCard = 'gptvoice.card'
+export const MenuIdCardDetail = 'gptvoice.cardDetail'
+export const MenuIdList = 'gptvoice.list'
 
 export interface MenuEntry {
   readonly args?: readonly string[]
@@ -13,96 +10,9 @@ export interface MenuEntry {
   readonly label: string
 }
 
-const menuEntryRefreshBoards: MenuEntry = {
-  command: 'gpt-voice.refreshBoards',
-  id: 'refreshBoards',
-  label: gpt-voiceStrings.refreshBoards(),
-}
-
-const menuEntrySignOut: MenuEntry = {
-  command: 'gpt-voice.logout',
-  id: 'signOut',
-  label: gpt-voiceStrings.signOut(),
-}
-
-const menuEntryBackToBoards: MenuEntry = {
-  command: 'gpt-voice.backToBoards',
-  id: 'backToBoards',
-  label: gpt-voiceStrings.backToBoards(),
-}
-
-const menuEntrySaveCard: MenuEntry = {
-  command: 'gpt-voice.saveCardDetail',
-  id: 'saveCard',
-  label: gpt-voiceStrings.saveCard(),
-}
-
-const menuEntryCloseCard: MenuEntry = {
-  command: 'gpt-voice.closeCardDetail',
-  id: 'closeCard',
-  label: gpt-voiceStrings.closeCard(),
-}
-
-const getAddCardEntry = (
-  state: Readonly<gpt-voiceViewState>,
-): readonly MenuEntry[] => {
-  if (!state.contextMenuListId) {
-    return []
-  }
-  return [
-    {
-      args: [state.contextMenuListId],
-      command: 'gpt-voice.startAddCard',
-      id: 'addCard',
-      label: gpt-voiceStrings.addCardMenu(),
-    },
-  ]
-}
-
-const getOpenCardEntry = (
-  state: Readonly<gpt-voiceViewState>,
-): readonly MenuEntry[] => {
-  if (!state.contextMenuCardId) {
-    return []
-  }
-  return [
-    {
-      args: [state.contextMenuCardId],
-      command: 'gpt-voice.openCard',
-      id: 'openCard',
-      label: gpt-voiceStrings.openCard(),
-    },
-  ]
-}
-
 export const getMenuEntries = (
-  state: Readonly<gpt-voiceViewState>,
+  state: Readonly<any>,
   menuId: string,
 ): readonly MenuEntry[] => {
-  switch (menuId) {
-    case MenuIdBoard:
-      return [menuEntryRefreshBoards, menuEntrySignOut]
-    case MenuIdCard:
-      return [
-        ...getOpenCardEntry(state),
-        ...getAddCardEntry(state),
-        menuEntryRefreshBoards,
-        menuEntryBackToBoards,
-      ]
-    case MenuIdCardDetail:
-      return [
-        menuEntrySaveCard,
-        menuEntryCloseCard,
-        menuEntryRefreshBoards,
-        menuEntryBackToBoards,
-      ]
-    case MenuIdList:
-      return [
-        ...getAddCardEntry(state),
-        menuEntryRefreshBoards,
-        menuEntryBackToBoards,
-      ]
-    default:
-      return []
-  }
+  return []
 }
