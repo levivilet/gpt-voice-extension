46cf2c0a3213eb7dec5c42c4c8a6ec3f84c35b5a
Le Vivilet <levivilet@pm.me>
2026-08-06 05:48:15 -0700

    import

--- packages/extension/src/parts/Main/Main.ts
+++ packages/extension/src/parts/Main/Main.ts
@@ -1,4 +1,11 @@
 import {} from '@lvce-editor/api'
+import {
+  activate as activateExtensionApi,
+  executeCommand,
+  registerCommand,
+  registerView,
+} from '@lvce-editor/api'
+
 const state = {
   isActivated: false,
 }
