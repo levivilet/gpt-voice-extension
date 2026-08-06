43ae2cb6a769ae8cd362334899f0a3399ad998c4
Le Vivilet <levivilet@pm.me>
2026-08-06 05:45:55 -0700

    unused

--- packages/extension/src/parts/Main/Main.ts
+++ packages/extension/src/parts/Main/Main.ts
@@ -1,28 +1,3 @@
-import {
-  activate as activateExtensionApi,
-  executeCommand,
-  registerCommand,
-  registerView,
-} from '@lvce-editor/api'
-import {
-  createCacheCredentialStorage,
-  testCacheName as testCredentialCacheName,
-} from '../CredentialStorage/CredentialStorage.ts'
-import {
-  createCacheCurrentBoardStorage,
-  testCacheName as testCurrentBoardCacheName,
-} from '../CurrentBoardStorage/CurrentBoardStorage.ts'
-import {
-  createMockgpt-voiceClient,
-  type Mockgpt-voiceData,
-} from '../Mockgpt-voiceClient/Mockgpt-voiceClient.ts'
-import {
-  createCacheRecentBoardStorage,
-  testCacheName as testRecentBoardCacheName,
-} from '../RecentBoardStorage/RecentBoardStorage.ts'
-import { cleargpt-voiceTestCaches } from '../TestStorage/TestStorage.ts'
-import * as gpt-voiceView from '../gpt-voiceView/gpt-voiceView.ts'
-
 const state = {
   isActivated: false,
 }
@@ -33,67 +8,5 @@ export const activate = async (): Promise<void> => {
   }
   state.isActivated = true
   await activateExtensionApi()
-  registerView(gpt-voiceView.view)
-  registerCommand({
-    execute() {
-      return executeCommand('Layout.toggleSideBarView', gpt-voiceView.viewId)
-    },
-    id: 'gpt-voice.show',
-  })
-  registerCommand({
-    execute() {
-      return gpt-voiceView.cancelNewCardActivegpt-voiceViewInstance()
-    },
-    id: 'gpt-voice.cancelNewCard',
-  })
-  registerCommand({
-    execute() {
-      return gpt-voiceView.closeCardDetailActivegpt-voiceViewInstance()
-    },
-    id: 'gpt-voice.closeCardDetail',
-  })
-  registerCommand({
-    execute() {
-      return gpt-voiceView.closeBoardFilterActivegpt-voiceViewInstance()
-    },
-    id: 'gpt-voice.closeBoardFilter',
-  })
-  registerCommand({
-    execute(cardId: string) {
-      return gpt-voiceView.openCardActivegpt-voiceViewInstance(cardId)
-    },
-    id: 'gpt-voice.openCard',
-  })
-  registerCommand({
-    execute() {
-      return gpt-voiceView.saveCardDetailActivegpt-voiceViewInstance()
-    },
-    id: 'gpt-voice.saveCardDetail',
-  })
-  registerCommand({
-    execute(listId: string) {
-      return gpt-voiceView.startAddCardActivegpt-voiceViewInstance(listId)
-    },
-    id: 'gpt-voice.startAddCard',
-  })
-  registerCommand({
-    execute() {
-      return gpt-voiceView.submitNewCardActivegpt-voiceViewInstance()
-    },
-    id: 'gpt-voice.submitNewCard',
-  })
-  registerCommand({
-    execute(options: any) {
-      return gpt-voiceView.addList(options)
-    },
-    id: 'gpt-voice.addList',
-  })
-  registerCommand({
-    execute(options: any) {
-      return gpt-voiceView.openMockBoard(options)
-    },
-    id: 'gpt-voice.openMockBoard',
-  })
-  registerCommG
-
-export const deactivate = (): void => {}
+  registerView(gpt - voiceView.view)
+}
