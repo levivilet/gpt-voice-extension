a0002127d16dc7a63c25f61d31a18acb855c6bdd
Le Vivilet <levivilet@pm.me>
2026-08-06 18:01:33 +0200

    eevnt

--- packages/extension/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/extension/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -8,71 +8,8 @@ interface DomEventListener {
 export const renderEventListeners = (): readonly DomEventListener[] => {
   return [
     {
-      name: 'handleImageError',
-      params: ['handleImageError', 'event.target.name'],
-    },
-    {
-      name: 'handleDragStart',
-      params: ['handleDragStart', 'event.target.name'],
-    },
-    {
-      name: 'handleDragEnd',
-      params: ['handleDragEnd'],
-    },
-    {
-      name: 'handleDragOver',
-      params: ['handleDragOver', 'event.currentTarget.dataset.id'],
-      preventDefault: true,
-    },
-    {
-      name: 'handleDragLeave',
-      params: ['handleDragLeave'],
-    },
-    {
-      name: 'handleDrop',
-      params: [
-        'handleDrop',
-        'event.currentTarget.dataset.id',
-        'event.dataTransfer.files',
-      ],
-      preventDefault: true,
-    },
-    {
-      name: 'handleKeyDown',
-      params: [
-        'handleKeyDown',
-        'event.target.name',
-        'event.key',
-        'event.ctrlKey',
-      ],
-    },
-    {
-      name: 'handleSashPointerDown',
-      params: ['handleSashPointerDown', 'event.clientX'],
-      trackPointerEvents: ['handleSashPointerMove', 'handleSashPointerUp'],
-    },
-    {
-      name: 'handleCardLabelPickerPointerDown',
-      params: ['handleCardLabelPickerPointerDown'],
-      preventDefault: true,
-    },
-    {
-      name: 'handleAddCardActionPointerDown',
-      params: ['handleAddCardActionPointerDown'],
-      preventDefault: true,
-    },
-    {
-      name: 'handleCardDescriptionCancelPointerDown',
-      params: ['handleCardDescriptionCancelPointerDown'],
-      preventDefault: true,
-    },
-    {
-      name: 'handleSashPointerMove',
-      params: ['handleSashPointerMove', 'event.clientX'],
-    },
-    {
-      name: 'handleSashPointerUp',
-      params: ['handleSashPointerUp'],
+      name: 'handleClickStart',
+      params: ['handleClickStart'],
     },
   ]
 }
