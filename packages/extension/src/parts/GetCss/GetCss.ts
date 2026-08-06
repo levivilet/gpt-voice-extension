588fdce2c201810d589065bbd5343381f5e57771
Le Vivilet <levivilet@pm.me>
2026-08-06 15:12:19 +0200

    fix

--- packages/extension/src/parts/GetCss/GetCss.ts
+++ packages/extension/src/parts/GetCss/GetCss.ts
@@ -1,20 +1,9 @@
-import type { gpt-voiceViewState } from '../gpt-voiceViewState/gpt-voiceViewState.ts'
-import { getBoardBackgroundCss } from '../BoardBackground/BoardBackground.ts'
-
-const getCardDetailCss = (state: Readonly<gpt-voiceViewState>): string => {
-  return `.gpt-voiceCardDetailPanel {
-  --gpt-voiceCardDetailWidth: ${state.cardDetailWidth}px;
+const getCardDetailCss = (state: Readonly<any>): string => {
+  return `.gptvoiceCardDetailPanel {
+  --gptvoiceCardDetailWidth: ${state.cardDetailWidth}px;
 }`
 }
 
-export const getCss = (state: Readonly<gpt-voiceViewState>): string => {
-  const boardBackgroundCss = state.boardDetail
-    ? getBoardBackgroundCss(
-        state.boardDetail.board,
-        state.boardBackgroundEnabled,
-      )
-    : ''
-  return [getCardDetailCss(state), boardBackgroundCss]
-    .filter(Boolean)
-    .join('\n\n')
+export const getCss = (state: Readonly<any>): string => {
+  return ''
 }
