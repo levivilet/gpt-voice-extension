d2631994f8931f9620852a92c6039d27ea6b9ab9
Le Vivilet <levivilet@pm.me>
2026-08-06 15:08:50 +0200

    e2e

--- packages/e2e/src/trello.virtual-dom-view.user-select-text.ts
+++ packages/e2e/src/trello.virtual-dom-view.user-select-text.ts
@@ -1,16 +1,8 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
-import {
-  createBoards,
-  createMockData,
-  useMockDataAndShowgpt-voice,
-} from './_gpt-voice.virtual-dom-view.shared.ts'
 
 export const name = 'gpt-voice.virtual-dom-view.user-select-text'
 
 export const test: Test = async ({ Command, expect, Locator }) => {
-  const boards = createBoards(1)
-  await useMockDataAndShowgpt-voice(Command, createMockData(boards))
-
   const welcomeText = Locator('.gpt-voiceWelcomeText')
   await expect(welcomeText).toBeVisible()
   await expect(welcomeText).toHaveCSS('user-select', 'text')
