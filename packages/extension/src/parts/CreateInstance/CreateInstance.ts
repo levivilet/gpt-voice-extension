607434d42c4d066646c28f48e5acee5fb55d3034
Le Vivilet <levivilet@pm.me>
2026-08-07 11:16:48 +0200

    add css

--- packages/extension/src/parts/CreateInstance/CreateInstance.ts
+++ packages/extension/src/parts/CreateInstance/CreateInstance.ts
@@ -55,7 +55,9 @@ export const createInstance = async (
       return {}
     },
     getCss() {
-      return ''
+      return `.GptVoice {
+--GptVoiceBubbleTransform: scale(${state.animationScale});
+}`
     },
     getMenuEntries() {
       return []
