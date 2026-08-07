9e390833366af4e5682d80524eff3a229a24d525
Le Vivilet <levivilet@pm.me>
2026-08-07 12:05:25 +0200

    feature: add readLevel

---
+++ packages/extension/src/parts/ReadLevel/ReadLevel.ts
@@ -0,0 +1,8 @@
+export const readLevel = (data: Uint8Array): number => {
+  let sumSquares = 0
+  for (let i = 0; i < data.length; i++) {
+    const v = (data[i] - 128) / 128
+    sumSquares += v * v
+  }
+  return Math.sqrt(sumSquares / data.length) // ~0 (silence) to ~0.6 (loud)
+}
