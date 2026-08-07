9dfdf87b31754a342c345dd46f1687f8489623bc
Le Vivilet <levivilet@pm.me>
2026-08-07 10:57:15 +0200

    e2e

---
+++ packages/e2e/src/gpt-voice-in-progress-stage.ts
@@ -0,0 +1,24 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'gpt-voice.in-progress-stage'
+
+export const test: Test = async ({
+  Command,
+  expect,
+  Layout,
+  Locator,
+  SideBar,
+}) => {
+  // arrange
+  await SideBar.open('gpt-voice.views.default')
+  const button = Locator('.GptVoiceButton')
+  await expect(button).toBeVisible()
+  await Command.executeExtensionCommand('GptVoice.setIsTest')
+
+  // act
+  await Command.executeExtensionCommand('GptVoice.handleClickStart')
+
+  // assert
+  const listeningBubble = Locator('.GptVoiceBubble.listening')
+  await expect(listeningBubble).toBeVisible()
+}
